2682c9073b33b8aaa920b54c268b009ac80a4f83
tai-martins <tainara@gec.inatel.br>
2022-06-01 21:44:14 -0300

    atualizando

--- src/main/java/br/inatel/cdg/observer/Observador.java
+++ src/main/java/br/inatel/cdg/observer/Observador.java
@@ -3,6 +3,13 @@ package br.inatel.cdg.observer;
 public class Observador implements iObservador {
 
     private String frase;
+    int id;
+
+    public Observador(int id) {
+        this.id = id;
+    }
+
+    String teste = "Testando Observer para a tarefa de engenharia de software";
 
     @Override
     public void update(Observavel frase){
--- src/main/java/br/inatel/cdg/observer/Observavel.java
+++ src/main/java/br/inatel/cdg/observer/Observavel.java
@@ -2,10 +2,13 @@ package br.inatel.cdg.observer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class Observavel implements iObservavel{
 
     private List<Observador> observadores;
+    private String frase;
+    private String separando[];
 
     public Observavel(){
         observadores = new ArrayList<Observador>();
@@ -33,11 +36,40 @@ public class Observavel implements iObservavel{
         notificaObservadores();
     }
 
+    public int qtdpalavras() {
+        String myString = this.frase;
+        StringTokenizer stringTokenizer1 = new StringTokenizer(myString);
+        int cont  = stringTokenizer1.countTokens();
+        return Integer.valueOf(cont);
+    }
+
+    public String[] separando() {
+        String myStr = this.frase;
+        String[] palavras = myStr.split(" ");
+        return palavras;
+    }
+
     public List<Observador> getObservadores() {
         return observadores;
     }
 
     public void setObservadores(List<Observador> observadores) {
         this.observadores = observadores;
+    }
+
+    public String getFrase() {
+        return frase;
+    }
+
+    public void setFrase(String frase) {
+        this.frase = frase;
+    }
+
+    public String[] getSeparando() {
+        return separando;
+    }
+
+    public void setSeparando(String[] separando) {
+        this.separando = separando;
     }
 }
